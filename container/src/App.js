cb4e36d52864439b0a65aedd22824534d8bb3d2e
Guilherme <guilherme.mendes@anbetec.com.br>
2025-02-11 07:03:51 -0300

    feat: adiciona componente AuthApp no roteamento do container

--- container/src/App.js
+++ container/src/App.js
@@ -1,5 +1,5 @@
-import React, { Suspense } from 'react';
-import { Router } from 'react-router-dom';
+import React, { lazy, Suspense, useState } from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
 import {
   StylesProvider,
   createGenerateClassName,
@@ -9,6 +9,8 @@ import { createBrowserHistory } from 'history';
 import Progress from './components/Progress';
 import Header from './components/Header';
 
+const AuthLazy = lazy(() => import('./components/AuthApp'));
+
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 });
@@ -16,13 +18,22 @@ const generateClassName = createGenerateClassName({
 const history = createBrowserHistory();
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
   return (
     <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header />
+          <Header
+            onSignOut={() => setIsSignedIn(false)}
+            isSignedIn={isSignedIn}
+          />
           <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
+            </Switch>
           </Suspense>
         </div>
       </StylesProvider>
